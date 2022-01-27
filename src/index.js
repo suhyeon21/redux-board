47f201936917f6ce1682d6d066ef102869be9a45
suhyeon21 <suhyeon.kan@gmail.com>
2022-01-27 16:14:13 +0900

    fix: 필요없는 내용 삭제

--- src/index.js
+++ src/index.js
@@ -9,19 +9,12 @@ import board_reducer from "./App_reducer";
 
 let store = createStore(
   board_reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), //??
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-//forest71.tistory.com/184?category=683254 [SW 개발이 좋은 사람]
-
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
