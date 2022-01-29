b474c3b998493410469e4cbc5e7ae28829e52810
suhyeon21 <suhyeon.kan@gmail.com>
2022-01-29 12:32:13 +0900

    refactor: redux-actions 활용하여 더 간단하게 작성

--- src/App_reducer.js
+++ src/App_reducer.js
@@ -1,24 +1,15 @@
+import { createAction, handleActions } from "redux-actions";
+
+//action type
 const BOARD_SAVE = "SAVE";
 const BOARD_REMOVE = "REMOVE";
 const BOARD_READ = "ONE";
-const BOARD_LIST = "LIST"; //CRUD 개념상 정의한 것이고 여기서 쓰진 않을 것
+const BOARD_LIST = "LIST";
 
-export const board_save = (data) => ({
-  type: BOARD_SAVE,
-  data,
-});
-
-export const board_remove = (brdno) => ({
-  type: BOARD_REMOVE,
-  brdno: brdno,
-});
-
-export const board_read = (brdno) => ({
-  type: BOARD_READ,
-  brdno: brdno,
-});
-
-export const board_list = () => ({ type: BOARD_LIST });
+export const board_save = createAction(BOARD_SAVE);
+export const board_remove = createAction(BOARD_REMOVE, (brdno) => brdno); //다른 함수들처럼 brdno 받는 거 생략 가능
+export const board_read = createAction(BOARD_READ);
+export const board_list = createAction(BOARD_LIST);
 
 const initialState = {
   maxNo: 3,
@@ -36,46 +27,83 @@ const initialState = {
       brddate: new Date(),
     },
   ],
-  selectedBoard: {}, //??
+  selectedBoard: {},
 };
 
-export default function board_reducer(state = initialState, action) {
-  let boards = state.boards;
-
-  switch (action.type) {
-    case BOARD_SAVE:
-      let data = action.data;
-      let maxNO = state.maxNO;
+export default handleActions(
+  {
+    [BOARD_SAVE]: (state, { payload: data }) => {
+      let boards = state.boards;
       if (!data.brdno) {
-        //new:Insert
+        let maxNo = state.maxNo;
         return {
           maxNo: maxNo + 1,
-          boards: boards.concat({ ...data, brdno: maxNo, brddate: new data() }),
-          selectedBoard: {},
+          boards: boards.concat({ ...data, brdno: maxNo, brddate: new Date() }),
+          selectBoard: {},
+        };
+      } else {
+        return {
+          ...state,
+          boards: boards.map((row) =>
+            data.brdno === row.brdno ? { ...data } : row,
+          ),
+          selectBoard: {},
         };
       }
+    },
+    [BOARD_REMOVE]: (state, { payload: brdno }) => {
+      let boards = state.boards;
+      return {
+        ...state,
+        boards: boards.filter((row) => row.brdno !== brdno),
+        selectBoard: {},
+      };
+    },
+    [BOARD_READ]: (state, { payload: brdno }) => {
+      let boards = state.boards;
+      return {
+        ...state,
+        selectedBoard: boards.find((row) => row.brdno === brdno),
+      };
+    },
+  },
+  initialState,
+);
 
-      return {
-        ...state,
-        boards: boards.map((row) =>
-          data.brdno === row.brdno ? { ...data } : row,
-        ),
-        selectedBoard: {},
-      };
-      console.log(...state);
+// export default function board_reducer(state = initialState, action) {
+//   let boards = state.boards;
 
-    case BOARD_REMOVE: //삭제할 행을 찾아서 지우는 방식 아니고 삭제할 행 빼고 다시 배열 만드는 방식
-      return {
-        ...state,
-        boards: boards.filter((row) => row.brdno !== action.brdno),
-        selectedBoard: {},
-      };
-    case BOARD_READ:
-      return {
-        ...state,
-        selectedBoard: boards.find((row) => row.brdno === action.brdno),
-      };
-    default:
-      return state;
-  }
-}
+//   switch (action.type) {
+//     case BOARD_SAVE:
+//       let data = action.data;
+//       let maxNo = state.maxNo;
+//       if (!data.brdno) {
+//         // new : Insert
+//         return {
+//           maxNo: maxNo + 1,
+//           boards: boards.concat({ ...data, brdno: maxNo, brddate: new Date() }),
+//           selectedBoard: {},
+//         };
+//       }
+//       return {
+//         ...state,
+//         boards: boards.map((row) =>
+//           data.brdno === row.brdno ? { ...data } : row,
+//         ),
+//         selectedBoard: {},
+//       };
+//     case BOARD_REMOVE:
+//       return {
+//         ...state,
+//         boards: boards.filter((row) => row.brdno !== action.brdno),
+//         selectedBoard: {},
+//       };
+//     case BOARD_READ:
+//       return {
+//         ...state,
+//         selectedBoard: boards.find((row) => row.brdno === action.brdno),
+//       };
+//     default:
+//       return state;
+//   }
+// }
